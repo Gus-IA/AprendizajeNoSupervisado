fd206d2d38053ed1a0f7fccdbfe8aaac600fc19b
Gus <garciagus1992@gmail.com>
2025-09-09 10:35:25 +0200

    add DBSCAN

--- aprendizaje_no_supervisado.py
+++ aprendizaje_no_supervisado.py
@@ -10,6 +10,9 @@ from sklearn.metrics import silhouette_score
 from sklearn.metrics import silhouette_samples
 from matplotlib.ticker import FixedLocator, FixedFormatter
 import matplotlib as mpl
+from sklearn.datasets import make_moons
+from sklearn.cluster import DBSCAN
+from sklearn.neighbors import KNeighborsClassifier
 
 # ---- K-Means ----
 
@@ -241,4 +244,79 @@ for k in (3, 4, 5, 6):
     plt.axvline(x=silhouette_scores[k - 2], color="red", linestyle="--")
     plt.title("$k={}$".format(k), fontsize=16)
 
+plt.show()
+
+
+# ---- DBSCAN ----
+
+# generamos unos datos con make_moons
+X, y = make_moons(n_samples=1000, noise=0.05, random_state=42)
+
+# instanciamos el modelo DBSCAN y lo entrenamos
+dbscan = DBSCAN(eps=0.05, min_samples=5)
+dbscan.fit(X)
+
+# cantidad de etiquetas encontradas
+print(np.unique(dbscan.labels_))
+
+
+# entrenamos otro modelo DBSCAN pero con un epsilon mayor
+dbscan2 = DBSCAN(eps=0.2)
+dbscan2.fit(X)
+
+print(np.unique(dbscan2.labels_))
+
+# mostramos ambos gráficos para compararlos
+def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
+    core_mask = np.zeros_like(dbscan.labels_, dtype=bool)
+    core_mask[dbscan.core_sample_indices_] = True
+    anomalies_mask = dbscan.labels_ == -1
+    non_core_mask = ~(core_mask | anomalies_mask)
+
+    cores = dbscan.components_
+    anomalies = X[anomalies_mask]
+    non_cores = X[non_core_mask]
+    
+    plt.scatter(cores[:, 0], cores[:, 1],
+                c=dbscan.labels_[core_mask], marker='o', s=size, cmap="Paired")
+    plt.scatter(cores[:, 0], cores[:, 1], marker='*', s=20, c=dbscan.labels_[core_mask])
+    plt.scatter(anomalies[:, 0], anomalies[:, 1],
+                c="r", marker="x", s=100)
+    plt.scatter(non_cores[:, 0], non_cores[:, 1], c=dbscan.labels_[non_core_mask], marker=".")
+    if show_xlabels:
+        plt.xlabel("$x_1$", fontsize=14)
+    else:
+        plt.tick_params(labelbottom=False)
+    if show_ylabels:
+        plt.ylabel("$x_2$", fontsize=14, rotation=0)
+    else:
+        plt.tick_params(labelleft=False)
+    plt.title("eps={:.2f}, min_samples={}".format(dbscan.eps, dbscan.min_samples), fontsize=14)
+
+plt.figure(figsize=(9, 3.2))
+
+plt.subplot(121)
+plot_dbscan(dbscan, X, size=100)
+
+plt.subplot(122)
+plot_dbscan(dbscan2, X, size=600, show_ylabels=False)
+
+plt.show()
+
+# instanciamos y entrenamos el modelo KNeighborsClassifier para clasificar diferentes muestras
+knn = KNeighborsClassifier(n_neighbors=50)
+knn.fit(dbscan2.components_, dbscan2.labels_[dbscan2.core_sample_indices_])
+
+
+# buscamos los clústers para estos datos
+X_new = np.array([[-0.5, 0], [0, 0.5], [1, -0.1], [2, 1]])
+knn.predict(X_new)
+
+# mostramos la probabilidad
+print(knn.predict_proba(X_new))
+
+# mostramos el gráfico de la frontera de decisión
+plt.figure(figsize=(6, 3))
+plot_decision_boundaries(knn, X, show_centroids=False)
+plt.scatter(X_new[:, 0], X_new[:, 1], c="b", marker="+", s=200, zorder=10)
 plt.show()
